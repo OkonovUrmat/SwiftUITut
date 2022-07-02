3e8dd27db0abbde07f6bff7e1b36a33e5bf98806
OkonovUrmat <okonov.okonov@gmail.com>
2022-07-02 16:00:56 +0600

    Initial Commit

---
+++ SwiftUITut/SwiftUITutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutApp.swift
+//  SwiftUITut
+//
+//  Created by Urmat on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
